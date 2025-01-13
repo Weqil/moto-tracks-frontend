0bebbaea004e882fa54511d5b1d94e7e80506977
Arrsenii <114972938+Weqil@users.noreply.github.com>
2025-01-13 16:39:34 +0500

    add component cabinet

---
+++ src/app/Pages/Profile/cabinet/cabinet.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabinet',
+  templateUrl: './cabinet.component.html',
+  styleUrls: ['./cabinet.component.scss'],
+})
+export class CabinetComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
