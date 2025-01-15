2ac2d3d3208a672daa9a2c931ff8087616c892f4
Arrsenii <114972938+Weqil@users.noreply.github.com>
2025-01-15 12:14:06 +0500

    add standart input

--- src/app/Shared/Components/Forms/standart-input/standart-input.component.ts
+++ src/app/Shared/Components/Forms/standart-input/standart-input.component.ts
@@ -1,14 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { MaskitoMask, MaskitoOptions,  } from '@maskito/core';
+import { SharedModule } from 'src/app/Shared/Modules/shared/shared.module';
+import { MaskitoDirective  } from '@maskito/angular'
 @Component({
   selector: 'app-standart-input',
   templateUrl: './standart-input.component.html',
   styleUrls: ['./standart-input.component.scss'],
+  imports: [SharedModule, MaskitoDirective]
 })
-export class StandartInputComponent  implements OnInit {
+export class StandartInputComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() control?: any
+  @Input() type: string = ''
+  @Input() label: string = ''
+  @Input() placeholder: string = ''
+  @Input() readonly: boolean = false
+  @Input() maskType!: string
+  @Input() openPassword: boolean = false
+  @Input() invalid: boolean = false
+  @Input() disabled: boolean = false
+  mask: MaskitoMask = new RegExp('')
+  optionMask: MaskitoOptions = {
+    mask: this.mask,
+  }
+  @Input() errorMessage: string = ''
+  @Output() changeInput: EventEmitter<string> = new EventEmitter()
+  emitInput(event: any): void {
+    this.changeInput.emit(event)
+  }
 
-  ngOnInit() {}
+  renderMask() {
+    if (this.maskType == 'phone') {
+      console.log('маска для телефона')
+      this.optionMask = {
+        mask: ['+', '7', ' ', '(', /\d/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/],
+      }
+    } else {
+    }
+  }
 
+  ngOnChanges() {}
+  openPasword(input: any) {
+    input.type = input.type === 'password' ? 'text' : 'password'
+  }
+  ngOnInit() {
+    this.renderMask()
+  }
 }
+function maskitoPhoneOptionsGenerator(arg0: {
+  countryIsoCode: string
+  metadata: any
+}): import('@maskito/core').MaskitoMask {
+  throw new Error('Function not implemented.')
+}
+
--- src/app/Shared/Modules/shared/shared.module.ts
+++ src/app/Shared/Modules/shared/shared.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Input, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonContent } from '@ionic/angular/standalone';
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
   
   ],
   imports: [
     IonContent,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     IonContent,
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
 
 })
