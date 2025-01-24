bd6b173aa491141bb80fe27fcb53afc19c45d3cf
MoskowPsix <bolshe.kivi@gmail.com>
2025-01-24 18:18:52 +0500

    add: register page

--- src/app/Shared/Data/Services/Auth/register.service.ts
+++ src/app/Shared/Data/Services/Auth/register.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Login } from '../../Interfaces/login-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
+  private readonly http:HttpClient = inject(HttpClient)
+
 
   constructor() { }
+
+  registerUser( registerForm:FormData){
+      return this.http.post<Login>(`${environment.BACKEND_URL}:${environment.BACKEND_PORT}/api/register`, registerForm)
+  }
 }
