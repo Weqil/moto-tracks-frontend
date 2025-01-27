e1b1ae73f7067bc5ea812378bcef18e7ac35c4ed
Arrsenii <114972938+Weqil@users.noreply.github.com>
2025-01-27 11:55:38 +0500

    fix toasts and loadings

--- src/app/Shared/Services/loading.service.ts
+++ src/app/Shared/Services/loading.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import { LoadingController } from '@ionic/angular'
+import { LoadingController } from '@ionic/angular/standalone'
 import { MessagesLoading } from '../Enums/messages-loading'
 
 @Injectable({
