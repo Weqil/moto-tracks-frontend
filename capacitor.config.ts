74919512fd23d5fd86411f6f400b8ac21025e328
MoskowPsix <bolshe.kivi@gmail.com>
2025-01-31 11:19:37 +0500

    fix add ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'moto-tracks-frontend',
+  appId: 'com.policam.motokros',
+  appName: 'motokros',
   webDir: 'www/browser'
 };
 
