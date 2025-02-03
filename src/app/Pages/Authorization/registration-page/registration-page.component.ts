afaf70d2d72aaee8f0e33aa90cff3e968bf606f5
Arrsenii <114972938+Weqil@users.noreply.github.com>
2025-02-03 15:20:24 +0500

    add validation register

--- src/app/Pages/Authorization/registration-page/registration-page.component.ts
+++ src/app/Pages/Authorization/registration-page/registration-page.component.ts
@@ -35,10 +35,10 @@ export class RegistrationPageComponent  implements OnInit {
     registerForm: FormGroup = new FormGroup({
       name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
       password_confirmation: new FormControl('', [Validators.required, Validators.minLength(3)]),
     })
-    registerInvalid = {
+    registerInvalid:any = {
       localError: false,
       serverError: false,
       name: {
@@ -60,38 +60,58 @@ export class RegistrationPageComponent  implements OnInit {
     }
 
     validateForm() {
-    this.registerInvalid.localError = false
-    if (this.registerForm.get('name')?.errors) {
-      this.registerInvalid.localError = true
-      this.registerInvalid.name.status = true
-      this.registerInvalid.name.message = this.registerForm.get('name')?.hasError('required')
-        ? 'Поле не может быть пустым'
-        : 'Имя должно быть не менее 3 символов'
-    } else {
-      this.registerInvalid.name.status = false
-      this.registerInvalid.name.message = ''
-    }
+      this.registerInvalid.localError = false
+      if (this.registerForm.get('name')?.errors) {
+        this.registerInvalid.localError = true
+        this.registerInvalid.name.status = true
+        this.registerInvalid.name.message = this.registerForm.get('name')?.hasError('required')
+          ? 'Поле не может быть пустым'
+          : 'Имя должно быть не менее 3 символов'
+      } else {
+        this.registerInvalid.name.status = false
+        this.registerInvalid.name.message = ''
+      }
 
-    if (this.registerForm.get('password')?.errors) {
-      this.registerInvalid.localError = true
-      this.registerInvalid.password.status = true
-      this.registerInvalid.password.message = this.registerForm.get('password')?.hasError('required')
-        ? 'Поле не может быть пустым'
-        : 'Пароль должен быть не менее 8 символов'
-    } else {
-      this.registerInvalid.password.status = false
-      this.registerInvalid.password.message = ''
-    }
-    if (this.registerForm.get('password')?.errors) {
-      this.registerInvalid.localError = true
-      this.registerInvalid.password.status = true
-      this.registerInvalid.password.message = this.registerForm.get('password')?.hasError('required')
-        ? 'Поле не может быть пустым'
-        : 'Пароль должен быть не менее 8 символов'
-    } else {
-      this.registerInvalid.password.status = false
-      this.registerInvalid.password.message = ''
-    }
+      if (this.registerForm.get('email')?.errors) {
+        this.registerInvalid.localError = true
+        this.registerInvalid.email.status = true
+        if( this.registerForm.get('email')?.hasError('required')){
+          this.registerInvalid.email.message = 'Поле не может быть пустым'
+        }
+        else if(this.registerForm.get('email')?.hasError('minLength')){
+          this.registerInvalid.email.message = 'E-mail должен быть не менее 3 символов'
+        }
+        else{
+          this.registerInvalid.email.message = 'E-mail некоректный'
+        }
+      }else{
+        this.registerInvalid.email.status = false
+        this.registerInvalid.email.message = ''
+      }
+
+      if (this.registerForm.get('password')?.errors) {
+        this.registerInvalid.localError = true
+        this.registerInvalid.password.status = true
+        this.registerInvalid.password.message = this.registerForm.get('password')?.hasError('required')
+          ? 'Поле не может быть пустым'
+          : 'Пароль должен быть не менее 8 символов'
+      } else {
+        this.registerInvalid.password.status = false
+        this.registerInvalid.password.message = ''
+      }
+      
+      console.log(this.registerForm.get('password_confirmation')?.value == this.registerForm.get('password')?.value)
+      if(this.registerForm.get('password_confirmation')?.value !== this.registerForm.get('password')?.value){
+        console.log(this.registerForm.get('password_confirmation')?.value == this.registerForm.get('password')?.value)
+        this.registerInvalid.localError = true
+        this.registerInvalid.password_confirmation.status = true
+        this.registerInvalid.password.status = true
+        this.registerInvalid.password_confirmation.message = 'Пароли не совпадают'
+      }else{
+        this.registerInvalid.password_confirmation.status = false
+        this.registerInvalid.password_confirmation.message = ''
+      }
+     
   }
   createFormData() {
     const fd: FormData = new FormData()
@@ -101,10 +121,18 @@ export class RegistrationPageComponent  implements OnInit {
     fd.append('password_confirmation', this.registerForm.get('password_confirmation')?.value)
     return fd
   }
+  errorResponseAfterReg(err:any){
+    let message:any[] = err.error.errors
+    Object.keys(message).forEach((err:any)=>{
+      this.registerInvalid[err].status = true
+      this.registerInvalid[err].message = message[err][0]
+    })
+    this.registerInvalid.serverError = true
+  }
   register(data: FormData): void {
     this.registerService.registerUser(data).pipe(
       catchError((err: serverError) => {
-        // this.errorResponseAfterLogin(err)
+        this.errorResponseAfterReg(err)
         // this.errorText = err
         this.loading.hideLoading()
         return EMPTY
@@ -123,9 +151,12 @@ export class RegistrationPageComponent  implements OnInit {
 
     submitLoginForm() {
       this.validateForm()
-      this.loading.showLoading()
-      const fd:FormData = this.createFormData()
-      this.register(fd)
+      if (!this.registerInvalid.localError) {
+        this.loading.showLoading()
+        const fd:FormData = this.createFormData()
+        this.register(fd)
+      }
+
   
     }
 
