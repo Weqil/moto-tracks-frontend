efcc8b18177c6cc929da40c353fa3a596698572a
Arrsenii <114972938+Weqil@users.noreply.github.com>
2025-02-06 12:37:41 +0500

    add change

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.policam.motokros',
   appName: 'motokros',
-  webDir: 'www/browser'
+  webDir: 'www/browser',
+
 };
 
 export default config;
