0db23f1b1bab5d92ce105bb9379cc570b8e90628
MoskowPsix <bolshe.kivi@gmail.com>
2025-02-10 16:06:12 +0500

    add: download file page add: hasRole Admin and Root Guard

--- src/app/Shared/Data/Enums/erros.ts
+++ src/app/Shared/Data/Enums/erros.ts
@@ -1,5 +1,6 @@
 export enum AuthErrosMessages {
     lateAuthorization = 'Неудачная авторизации',
     emailNotCorrected = 'Значение поля email адрес должно быть действительным электронным адресом.',
-    emailNotFound = 'Значение поля email адрес не существует.'
+    emailNotFound = 'Значение поля email адрес не существует.',
+    notPermission = 'Доступ запрещен.'
 }
