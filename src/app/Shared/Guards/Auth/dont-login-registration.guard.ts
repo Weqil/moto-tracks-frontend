e17f40f470bfa0d88d70cfba5d334798bf9463a1
weqil <ar5ipooshka@yandex.ru>
2025-02-19 14:42:15 +0500

    add guard on log and registr

---
+++ src/app/Shared/Guards/Auth/dont-login-registration.guard.ts
@@ -0,0 +1,13 @@
+import { inject } from "@angular/core"
+import { LoginService } from "../../Data/Services/Auth/login.service"
+import { AuthService } from "../../Data/Services/Auth/auth.service"
+import { NavController } from "@ionic/angular/standalone"
+import { UserService } from "../../Data/Services/User/user.service"
+export const canDontLoginRegistration = ()=>{
+    const navController:NavController = inject(NavController)
+    let isLoggedIn:boolean = inject(AuthService).isAuthenticated()
+    if(isLoggedIn && inject(UserService).user.value){
+        return navController.navigateForward('/cabinet')
+    } 
+   return true 
+} 
