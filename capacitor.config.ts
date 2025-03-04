fe54026c57009459b39fcb1a409cc5a6bf22827a
weqil <ar5ipooshka@yandex.ru>
2025-03-04 15:04:41 +0500

    add splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,19 @@ const config: CapacitorConfig = {
   appName: 'Мотокросс',
   webDir: 'www/browser',
   bundledWebRuntime: false,
+  plugins:{
+      SplashScreen: {
+        launchShowDuration: 1000,    // Время показа (мс)
+        backgroundColor: "#ADD8E6",  // Светло-голубой фон
+        showSpinner: false, 
+        iosScaleType: "FIT_XY",
+        androidScaleType: "FIT_XY", // Масштабирование картинки
+        androidSpinnerStyle: "large", // Размер спиннера (Android)
+        iosSpinnerStyle: "large", // Размер спиннера (iOS)
+        splashFullScreen: false,  // Полноэкранный режим
+        splashImmersive: true    // Скрывает системные панели
+      }
+    },
   // server: {
   //   url: 'https://dev-moto.vokrug.city',
   // },
