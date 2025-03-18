84067bdea3d432589209b23a0ccde9b96faa07b4
Arrsenii <114972938+Weqil@users.noreply.github.com>
2025-03-18 18:12:27 +0500

    test

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 chmod 777 -R www
 rm -r www/browser
+npm cache clean --force
+rm -rf node_modules package-lock.json
 npm install --force
 export NODE_OPTIONS="--max-old-space-size=8192"
 npm run build 
