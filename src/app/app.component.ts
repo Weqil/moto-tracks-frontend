dadff713fe88878b426f7dee8b49e2fb3e365983
weqil <ar5ipooshka@yandex.ru>
2025-03-24 16:48:36 +0500

    fix buf white skreen?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { App } from '@capacitor/app';
-
+import { Router } from '@angular/router';
 import { Component, inject } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, NavController } from '@ionic/angular/standalone';
 import { UserService } from './Shared/Data/Services/User/user.service';
 import { MetrikaModule } from 'ng-yandex-metrika';
-import  moment, { MomentInput } from 'moment'
+import  moment, { Moment, MomentInput } from 'moment'
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -12,29 +13,30 @@ import  moment, { MomentInput } from 'moment'
 })
 
 export class AppComponent {
-  private lastBackgroundTime: MomentInput  = moment()
-  constructor() {
+  private startTimeInBackground?: Moment;
+  private finishTimeInBackground?: Moment;
+  navController: NavController = inject(NavController)
+  constructor(private navCtrl: NavController, private router: Router) {
     
-    App.addListener('appStateChange', (state) => { 
-      //отслеживается состояние приложения в фоне оно или нет, если в фоне мы записываем этот 
-      //момент в переменную и при выходе из фона мы из текущего момента вычитаем переменную и если разница, нахождение в фоне 
-      //было более 2 минут то мы перезагружаем приложение
-      if (state.isActive) {
-        
-        let diff = moment().diff(moment(this.lastBackgroundTime), 'minutes');
-        // console.log(`Active - diff ${diff}`);
-        if(diff>=5){
-          // console.log(diff)
-          window.location.reload()
-          // console.log('Приложение обновилось спустя')
+    App.addListener('resume', () => { 
+      this.finishTimeInBackground = moment()
+        console.log(`Приложение снова активно`);
+        console.log(this.finishTimeInBackground.format('HH:mm:ss'))
+        if (this.startTimeInBackground) {
+          const diffInSeconds = this.finishTimeInBackground.diff(this.startTimeInBackground, 'seconds');
+          console.log(`Приложение было в фоновом режиме ${diffInSeconds} секунд`);
+          if(diffInSeconds>20){
+            this.navCtrl.navigateRoot(this.router.url);
+            console.log(`Отправили на ту же страницу`);
+          }
         }
-        
-      }else {
-        this.lastBackgroundTime = moment()
-        // console.log(`Приложение свернуто`);
-      }
-
     });
+
+    App.addListener('pause', () => { 
+      this.startTimeInBackground = moment()
+      console.log(`Приложение ушло в фон`);
+      console.log(this.startTimeInBackground.format('HH:mm:ss'))
+  });
   }
   
   userService:UserService = inject(UserService)
@@ -46,3 +48,17 @@ export class AppComponent {
     
   }
 }
+        // let diff = moment().diff(moment(this.lastBackgroundTime), 'seconds');
+        // console.log(`Active - diff ${diff}`);
+        // if(diff>=20){
+          // console.log(diff)
+          // window.location.reload()
+          // console.log('Приложение обновилось')
+        // }
+        
+      // }
+      // else if(state.) {
+      //   this.lastBackgroundTime = moment().format('HH:mm:ss');
+      //   console.log(`Приложение свернуто`);
+      //   console.log(this.lastBackgroundTime);
+      // }
