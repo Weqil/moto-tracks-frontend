72aa38fe0be5286a3e43d8bb2ba717e3e4a48505
weqil <ar5ipooshka@yandex.ru>
2025-03-24 15:21:09 +0500

    fix design command modal

--- src/app/Pages/Profile/user-documents/user-documents.component.ts
+++ src/app/Pages/Profile/user-documents/user-documents.component.ts
@@ -197,6 +197,7 @@ submitValidate(){
 
 getRegions(){
   this.mapService.getAllRegions().pipe().subscribe((res:any)=>{
+    this.searchRegionItems.push({name:`Россия`,value:''})
     res.data.forEach((region:any) => {
       this.searchRegionItems.push({
         name:`${region.name} ${region.type}`,
