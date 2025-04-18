caa53e9d39162665bab950a82525e14823d526d9
Arrsenii <114972938+Weqil@users.noreply.github.com>
2025-04-18 15:46:43 +0500

    add back button

--- src/app/Pages/linarik-ui/linarik-ui.component.ts
+++ src/app/Pages/linarik-ui/linarik-ui.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BackButtonComponent } from '@app/Shared/Components/UI/LinarikUI/buttons/back-button/back-button.component';
 import { IconButtonComponent } from '@app/Shared/Components/UI/LinarikUI/buttons/icon-button/icon-button.component';
 import { IonContent } from "@ionic/angular/standalone";
 
@@ -8,7 +9,8 @@ import { IonContent } from "@ionic/angular/standalone";
   styleUrls: ['./linarik-ui.component.scss'],
   imports:[
     IonContent,
-    IconButtonComponent
+    IconButtonComponent,
+    BackButtonComponent
   ]
 })
 export class LinarikUiComponent  implements OnInit {
---
+++ src/app/Shared/Components/UI/LinarikUI/buttons/back-button/back-button.component.ts
@@ -0,0 +1,54 @@
+import { NgClass } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-back-button',
+  templateUrl: './back-button.component.html',
+  styleUrls: ['./back-button.component.scss'],
+  imports:[NgClass]
+})
+export class BackButtonComponent  implements OnInit {
+
+  constructor() { }
+
+  /**
+  * Тема кнопки: `dark` — тёмная по умолчанию, `red` — красная, `white` — белая, `gray` - серая
+  */
+  @Input() theme: 'dark'|'gray'|'red'|'white' = 'gray'
+
+  /**
+  * Цвет текста
+  */
+  @Input() fontColor: 'font-dark'|'font-red'|'font-white' = 'font-white'
+
+   /**
+  * Обводка кнопки 
+  */
+  @Input() border:'red-border'|'gray-border'|'' = ''
+
+    /**
+   *Ширина кнопки
+   */
+   @Input() widht:'w80' = 'w80'
+
+  /**
+  * Кнопка будет выглядеть недоступной
+  */
+  @Input() disabled:boolean = false
+
+  /**
+  * Текст кнопки
+  */
+  @Input() buttonText:string ='Назад'
+
+  /**
+   * Функция собирает все классы и возвращает массив с нужными значениями
+   */
+
+    get getClasses():string[]{
+      return [this.theme, this.disabled ? 'disabled':'',this.widht, this.border, this.fontColor,'back-button']
+    }
+
+  ngOnInit() {}
+
+}
