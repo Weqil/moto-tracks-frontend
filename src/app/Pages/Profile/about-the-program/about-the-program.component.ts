e70b26aa0dbfa9816d17a2eef0d6de3e91dd7690
weqil <ar5ipooshka@yandex.ru>
2025-04-23 11:44:36 +0500

    about-the-program v2

--- src/app/Pages/Profile/about-the-program/about-the-program.component.ts
+++ src/app/Pages/Profile/about-the-program/about-the-program.component.ts
@@ -1,11 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { environment } from 'src/environments/environment';
+import { HeaderComponent } from "../../../Shared/Components/UI/header/header.component";
+import { IonContent } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-about-the-program',
   templateUrl: './about-the-program.component.html',
   styleUrls: ['./about-the-program.component.scss'],
+  imports: [IonContent, HeaderComponent],
 })
 export class AboutTheProgramComponent  implements OnInit {
 
