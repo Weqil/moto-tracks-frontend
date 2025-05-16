e2d1130a81ff72abeee8602f6a2540fcff5c0bed
Arrsenii <114972938+Weqil@users.noreply.github.com>
2025-05-16 18:47:04 +0500

    add not found

--- src/app/CommonUI/Pages/not-found-pages/not-found-pages.component.ts
+++ src/app/CommonUI/Pages/not-found-pages/not-found-pages.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { IconButtonComponent } from '@app/Shared/Components/UI/LinarikUI/buttons/icon-button/icon-button.component';
+import { IonModal, NavController, Platform } from '@ionic/angular/standalone';
 @Component({
   selector: 'app-not-found-pages',
   templateUrl: './not-found-pages.component.html',
   styleUrls: ['./not-found-pages.component.scss'],
+  imports:[IconButtonComponent]
 })
 export class NotFoundPagesComponent  implements OnInit {
 
   constructor() { }
-
+  navController:NavController = inject(NavController);
   ngOnInit() {}
+
+  back(){
+    this.navController.navigateForward('')
+  }
 
 }
