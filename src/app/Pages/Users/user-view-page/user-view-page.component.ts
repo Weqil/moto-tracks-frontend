c0f76f0546b218ed3bd5b25648320a72124b9efa
weqil <ar5ipooshka@yandex.ru>
2025-05-22 11:42:06 +0500

    add changes

--- src/app/Pages/Users/user-view-page/user-view-page.component.ts
+++ src/app/Pages/Users/user-view-page/user-view-page.component.ts
@@ -9,18 +9,24 @@ import { ActivatedRoute } from '@angular/router';
 import { CheckImgUrlPipe } from "../../../Shared/Helpers/check-img-url.pipe";
 import { IonCheckbox, IonModal, NavController, IonLabel } from '@ionic/angular/standalone';
 import { IconButtonComponent } from "../../../Shared/Components/UI/LinarikUI/buttons/icon-button/icon-button.component";
-
+import { CommonModule } from '@angular/common';
+import { CheckUserRoleService } from '@app/Shared/Data/Services/check-user-role.service';
+import { CommandSectionComponent } from "../../../Shared/Components/Commands/command-section/command-section.component";
+import { ICommand } from '@app/Shared/Data/Interfaces/command';
+import { ComandsService } from '@app/Shared/Data/Services/Comands/comands.service';
 
 @Component({
   selector: 'app-user-view-page',
   templateUrl: './user-view-page.component.html',
   styleUrls: ['./user-view-page.component.scss'],
-  imports: [HeaderModule, IonContent, CheckImgUrlPipe, IconButtonComponent],
+  imports: [HeaderModule, IonContent, CheckImgUrlPipe, IconButtonComponent, CommonModule, CommandSectionComponent],
 })
 export class UserViewPageComponent  implements OnInit {
 
   user!:User 
+  command!:ICommand
   userId!: string 
+  comandService: ComandsService = inject(ComandsService)
   userService: UserService = inject(UserService)
   loadingService: LoadingService = inject(LoadingService)
   route: ActivatedRoute = inject(ActivatedRoute)
@@ -28,6 +34,10 @@ export class UserViewPageComponent  implements OnInit {
   loaderService:LoadingService = inject(LoadingService)
   navController: NavController = inject(NavController)
   @Input() userIdGet!: string
+  userTranslitStatuses:string[] = []
+  selectedStatusItem!:any 
+  checkUserRole:CheckUserRoleService = inject(CheckUserRoleService)
+
 
   back(){
     this.navController.back()
@@ -37,6 +47,7 @@ export class UserViewPageComponent  implements OnInit {
 
   ngOnInit() {
 
+
     if(this.userIdGet){
       this.userId = this.userIdGet
       this.getUser()
@@ -44,24 +55,42 @@ export class UserViewPageComponent  implements OnInit {
       this.route.params.pipe(takeUntil(this.destroy$)).subscribe((params) => {
         this.userId = params['id']
         this.getUser()})
+        // this.getCommand(Number(this.userId))
     }
   
   }
   
+  // getCommand(rcomandId: number){
+  //   let loader:HTMLIonLoadingElement
+  //   this.loaderService.showLoading().then((res:HTMLIonLoadingElement)=>{
+  //     loader = res
+  //    })
+
+  //    this.comandService.getCommandById(rcomandId).pipe(
+  //     finalize(()=>{
+  //       this.loadingService.hideLoading(loader)  
+  //     })
+  //    ).subscribe((res:any)=>{
+  //     // this.command = res
+  //     console.log(res)
+  //    })
+
+  // }
+
   getUser(){
     let loader:HTMLIonLoadingElement
     this.loaderService.showLoading().then((res:HTMLIonLoadingElement)=>{
       loader = res
      })
 
-    
-    
     this.userService.getUserById(this.userId).pipe(
       finalize(()=>{
         this.loadingService.hideLoading(loader)  
       })
     ).subscribe((res:any) => {
       this.user = res.user
+      // this.comandId = res.user.personal.com
+      this.userTranslitStatuses = this.checkUserRole.getUserRoleNamesInTranslit(this.user)
       // console.log('emae2:')
       // console.log(this.user)
     })
