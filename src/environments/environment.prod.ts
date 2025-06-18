151ee4dc04bad0fda4d2cc4c81b3ee8d7628cc88
weqil <ar5ipooshka@yandex.ru>
2025-06-18 12:38:17 +0500

    add: the processing of the paid button - If a user has registered for a class that is not listed in the services, the button will not be displayed.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,35 +1,35 @@
-// export const environment = {
-//     production: true,
-//     apiKeyYandex: '9a1df530-ff4c-4c63-b19c-ede18479110d',
-//     apiKeyYandexSubject: '326052a4-78c1-44b0-8971-2f18fbde7cf9',
-//     YANDEX_METRICA_KEY: 96112606,
-//     YANDEX_METRICA_ON: true,
-//     APP_NAME: 'VOKRUG',
-//     BASE_URL: 'https://dev-moto.vokrug.city/',
-//     PORT: '443',
-//     BACKEND_URL: 'https://api.dev-moto.vokrug.city',
-//     metrikaId:0,
-//     BACKEND_PORT: '443',
-//     DOMAIN: 'mlcity.ru',
-//     appleClientId: 'mlcity.ru',
-//     appleAuthUrl: 'https://api.dev-moto.vokrug.city/api/social-auth/apple',
-//     vkontakteAuthUrl: `https://api.dev-moto.vokrug.city.city/api/social-auth/vkontakte`,
-//     yandexAuthUrl: 'https://api.vokrug.city/api/social-auth/yandex/',
-//     vkontakteServiceKey: '7c6d50047c6d50047c6d5004647f7f17fc77c6d7c6d50041fa47869a3cc84a7c0bed429',
-//     version: '0.0.1'
-// }
+export const environment = {
+    production: true,
+    apiKeyYandex: '9a1df530-ff4c-4c63-b19c-ede18479110d',
+    apiKeyYandexSubject: '326052a4-78c1-44b0-8971-2f18fbde7cf9',
+    YANDEX_METRICA_KEY: 96112606,
+    YANDEX_METRICA_ON: true,
+    APP_NAME: 'VOKRUG',
+    BASE_URL: 'https://dev-moto.vokrug.city/',
+    PORT: '443',
+    BACKEND_URL: 'https://api.dev-moto.vokrug.city',
+    metrikaId:0,
+    BACKEND_PORT: '443',
+    DOMAIN: 'mlcity.ru',
+    appleClientId: 'mlcity.ru',
+    appleAuthUrl: 'https://api.dev-moto.vokrug.city/api/social-auth/apple',
+    vkontakteAuthUrl: `https://api.dev-moto.vokrug.city.city/api/social-auth/vkontakte`,
+    yandexAuthUrl: 'https://api.vokrug.city/api/social-auth/yandex/',
+    vkontakteServiceKey: '7c6d50047c6d50047c6d5004647f7f17fc77c6d7c6d50041fa47869a3cc84a7c0bed429',
+    version: '0.0.1'
+}
  
 
 
-export const environment = {
-  production: true,
-  apiKeyYandex: '9a1df530-ff4c-4c63-b19c-ede18479110d',
-  apiKeyYandexSubject: '326052a4-78c1-44b0-8971-2f18fbde7cf9',
-  APP_NAME: 'VOKRUG',
-  BASE_URL: 'https://moto.vokrug.city/',
-  PORT: '443',
-  BACKEND_URL: 'https://api-moto.vokrug.city',
-  metrikaId: 100026583,
-  BACKEND_PORT: '443',
-  version: '0.1.1'
-}
+// export const environment = {
+//   production: true,
+//   apiKeyYandex: '9a1df530-ff4c-4c63-b19c-ede18479110d',
+//   apiKeyYandexSubject: '326052a4-78c1-44b0-8971-2f18fbde7cf9',
+//   APP_NAME: 'VOKRUG',
+//   BASE_URL: 'https://moto.vokrug.city/',
+//   PORT: '443',
+//   BACKEND_URL: 'https://api-moto.vokrug.city',
+//   metrikaId: 100026583,
+//   BACKEND_PORT: '443',
+//   version: '0.1.1'
+// }
