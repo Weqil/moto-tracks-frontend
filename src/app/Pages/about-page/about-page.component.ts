689312022bb8d3f5904c7b8218a087b963fdb892
Arrsenii <114972938+Weqil@users.noreply.github.com>
2025-06-23 18:01:22 +0500

    add links

--- src/app/Pages/about-page/about-page.component.ts
+++ src/app/Pages/about-page/about-page.component.ts
@@ -20,6 +20,19 @@ export class AboutPageComponent  implements OnInit {
   redirectInSelectAuth(){
     this.navController.navigateForward('/select-auth');
   }
+  redirectInLink(link:string){
+    let links:any = {
+      tg:'https://t.me/mototrackapp',
+      google: 'https://play.google.com/store/apps/details?id=com.policam.motokros',
+      appStore: 'https://apps.apple.com/us/app/%D0%BC%D0%BE%D1%82%D0%BE%D0%BA%D1%80%D0%BE%D1%81c/id6741205282',
+      ruStore:'https://www.rustore.ru/catalog/app/com.policam.motokros'
+    }
+    if (links[link]) {
+      window.open(links[link], '_blank');
+    } else {
+      console.error('Invalid link:', link);
+    }
+  }
   ngOnInit() {}
 
 }
