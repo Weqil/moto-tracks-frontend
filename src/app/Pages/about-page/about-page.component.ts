f2f8add83aaf63e43225395038a879c7133c7d38
Arrsenii <114972938+Weqil@users.noreply.github.com>
2025-06-23 14:40:40 +0500

    add button in about

--- src/app/Pages/about-page/about-page.component.ts
+++ src/app/Pages/about-page/about-page.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { IconButtonComponent } from '@app/Shared/Components/UI/LinarikUI/buttons/icon-button/icon-button.component';
+
+
 import { IonContent } from "@ionic/angular/standalone";
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
   imports: [
      IonContent,
+     IconButtonComponent
   ],
   styleUrls: ['./about-page.component.scss'],
 })
