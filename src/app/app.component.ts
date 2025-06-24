561a4fbbd2109709a27df6943f7a9eed73bad4fa
MoskowPsix <bolshe.kivi@gmail.com>
2025-06-24 15:43:47 +0500

    re

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,55 +53,6 @@ export class AppComponent {
   PushNotifications: any;
   constructor(private navCtrl: NavController, private router: Router) {
   }
-  initPushNotifications() {
-    console.log('Initializing HomePage');
-
-    // Request permission to use push notifications
-    // iOS will prompt user and return if they granted permission or not
-    // Android will just grant without prompting
-    PushNotifications.requestPermissions().then(result => {
-      if (result.receive === 'granted') {
-        // Register with Apple / Google to receive push via APNS/FCM
-        PushNotifications.register();
-      } else {
-        // Show some error
-      }
-    });
-
-    // On success, we should be able to receive notifications
-    PushNotifications.addListener('registration',
-      (token: Token) => {
-        this.toastService.showToast(token.value, 'success')
-        console.log(token.value)
-        // alert('Push registration success, token: ' + token.value);
-      }
-    );
-
-    // Some issue with our setup and push will not work
-    PushNotifications.addListener('registrationError',
-      (error: any) => {
-        this.toastService.showToast(error, 'error')
-        // alert('Error on registration: ' + JSON.stringify(error));
-      }
-    );
-
-    // Show us the notification payload if the app is open on our device
-    PushNotifications.addListener('pushNotificationReceived',
-      (notification: PushNotificationSchema) => {
-        this.toastService.showToast(JSON.stringify(notification), 'primary')
-
-        // alert('Push received: ' + JSON.stringify(notification));
-      }
-    );
-
-    // Method called when tapping on a notification
-    PushNotifications.addListener('pushNotificationActionPerformed',
-      (notification: ActionPerformed) => {
-        this.toastService.showToast(JSON.stringify(notification), 'primary')
-        // alert('Push action performed: ' + JSON.stringify(notification));
-      }
-    );
-  }
    onUpdate() {
     const platform = Capacitor.getPlatform();
 
@@ -165,11 +116,7 @@ export class AppComponent {
   cupService:CupService = inject(CupService)
 
   askForNotifications() {
-    if (Capacitor.getPlatform() === 'web') {
       this.fcmService.requestPermission();
-    } else if (Capacitor.getPlatform() === 'android') {
-      this.initPushNotifications();
-    }
   }
   ngOnInit() {
     // console.log(this.stringHaveCurrentWords('50 см3','Коляски 7 50 см3'))
@@ -187,10 +134,6 @@ export class AppComponent {
       this.cupService.allDegree.next(res.degree) 
     })
     // инициализация  push для web
-    if (Capacitor.getPlatform() === 'web') {
       this.fcmService.requestPermission();
-    } else if (Capacitor.getPlatform() === 'android') {
-      this.initPushNotifications();
-    }
   }
 }
