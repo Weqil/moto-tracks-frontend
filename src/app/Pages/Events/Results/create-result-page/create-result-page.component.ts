74e9c0b0ff97c0edf9ac0ec8910346b35f45b560
Weqil <ar5ipooshka@yandex.ru>
2025-07-21 14:37:05 +0500

    feat: add header in create result

--- src/app/Pages/Events/Results/create-result-page/create-result-page.component.ts
+++ src/app/Pages/Events/Results/create-result-page/create-result-page.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { HeaderComponent } from '@app/Shared/Components/UI/header/header.component'
+import { IonContent } from '@ionic/angular/standalone'
 
 @Component({
   selector: 'app-create-result-page',
   templateUrl: './create-result-page.component.html',
   styleUrls: ['./create-result-page.component.scss'],
+  imports: [IonContent, HeaderComponent],
 })
-export class CreateResultPageComponent  implements OnInit {
-
-  constructor() { }
-
+export class CreateResultPageComponent implements OnInit {
+  constructor() {}
+  back() {
+    console.log('back')
+  }
   ngOnInit() {}
-
 }
